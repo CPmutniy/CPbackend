3cd112151c377251f67a7469a9990cf64bed0e21
RuslanLinuxoid <sevrukovruslan35@gmail.com>
2020-08-22 08:33:21 +0300

    first models added

--- CP/zhkh/admin.py
+++ CP/zhkh/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Adress)
+admin.site.register(Person)
+admin.site.register(Voting)
+admin.site.register(Question)
+admin.site.register(Answer)
--- CP/zhkh/models.py
+++ CP/zhkh/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Adress(models.Model):
+    name = models.CharField(max_length=256)
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+
+
+class Person(models.Model):
+    surname = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    patronymic = models.CharField(max_length=256)
+    adress = models.ForeignKey(Adress, on_delete=models.CASCADE)
+    publick_key = models.BinaryField()
+
+
+class Voting(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+
+
+class Answer(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    answer = models.CharField(max_length=256)
+    signature = models.BinaryField()
