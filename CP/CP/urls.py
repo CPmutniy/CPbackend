dac03faf28fba36d067e0b9fa87b694319895b2a
RuslanLinuxoid <sevrukovruslan35@gmail.com>
2020-08-22 13:39:16 +0300

    fixed api

--- CP/CP/urls.py
+++ CP/CP/urls.py
@@ -8,6 +8,11 @@ from zhkh import views
 router = DefaultRouter()
 
 router.register('company', views.CompanyViewSet, basename='company')
+router.register('adress', views.AdressViewSet, basename='adress')
+router.register('person', views.PersonViewSet, basename='person')
+router.register('voting', views.VotingViewSet, basename='voting')
+router.register('question', views.QuestionViewSet, basename='question')
+router.register('answer', views.AnswerViewSet, basename='answer')
 
 urlpatterns = router.urls
 
